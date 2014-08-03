a4f9e07f31efb037fbb2ca7141970a2a1136c84a
Joel Pablo <joel.pablo@digitalmonkstudio.com>
2014-08-04 03:00:17 +0800

    added final test - login page

--- test/midway/site.spec.js
+++ test/midway/site.spec.js
@@ -9,7 +9,7 @@ describe("Express/Angular Zombie test", function() {
 
     before(function(){
         this.browser = new Browser({site:url, debug:false});
-        server = app.listen(3002);// listen to new port
+        server = app.listen(3002);// listen to new test port
      })
 
 
@@ -19,24 +19,51 @@ describe("Express/Angular Zombie test", function() {
         browser.visit('/');
             browser.wait(function(){
                  expect(browser.text(".home h2")).to.equal('Welcome Home');
-             //    console.log('wait done ..')
                 done();
             })
-        })
+        } )
      });
 
 
      describe("About Page", function() {
-       it("should display About ZombieJS Two (2) Contributors", function() {
+       it("should display About ZombieJS Two (2) Contributors", function(done) {
            var browser = this.browser;
            browser.clickLink(browser.querySelectorAll('a')[2]);
            browser.wait(function(){
                expect(browser.queryAll(".about-contributors li")).to.have.length(2);
-
                done();
            })
 
         });
+     });
+
+     describe("Login Page", function() {
+       it("should display Login Form", function(done) {
+        var browser = this.browser;
+            browser.clickLink(browser.querySelectorAll('a')[1]);
+            browser.wait(function () {
+                 expect(browser.queryAll("form")).to.have.length(1);
+
+                done();
+            })
+        });
+
+        it("should display error message if username and password is wrong", function(done){
+            var browser = this.browser;
+            browser.fill("Username","Jake").fill("Password","Weary"); // fill-in the fields
+            browser.pressButton(browser.button(".login-submit"));
+            expect(browser.text("span")).to.equal("Invalid username or password. Please try again.")
+            done();
+        });
+
+        it("should display success message if username and password is correct", function(done){
+            var browser = this.browser;
+            browser.fill("Username","test").fill("Password","test"); // fill-in the fields
+            browser.pressButton(browser.button(".login-submit"));
+            expect(browser.text("span")).to.equal("Welcome Back User!")
+            done();
+        });
+
      });
 
 
